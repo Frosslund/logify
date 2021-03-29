c704e07b9b8942f8b8ffdab477e842a7b4e31e09
vgez <vgez@kth.se>
2021-03-29 23:05:27 +0200

    login_v1_valen

--- src/views/Navbar.js
+++ src/views/Navbar.js
@@ -1,14 +1,16 @@
 import Button from './components/Button';
+import { handleLogin } from '../utils/handleLogin'
 
 const Navbar = () => {
-    return (
-        <header>
-            <h1>Logify</h1>
-            <Button color='red' text='Log'/>
-            <Button color='red' text='Lists'/>
-            <Button color='red' text='Profile'/>
-        </header>
-    );
+	return (
+		<header>
+			<h1>Logify</h1>
+			<Button color='red' text='Log'/>
+			<Button color='red' text='Lists'/>
+			<Button color='red' text='Profile'/>
+			<Button color='red' text='Login' type="submit" onClick={handleLogin} />
+		</header>
+	);
 }
 
 export default Navbar;
